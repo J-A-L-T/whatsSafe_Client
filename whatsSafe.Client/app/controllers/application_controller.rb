b5b9f4882b22bcfb0421307b6ad66446e63a5a62
HansWurst90 <Jan@mussenbrock-w.de>
2015-07-01 21:43:36 +0200

    Fehler beim initialen Start gefixt

--- whatsSafe.Client/app/controllers/application_controller.rb
+++ whatsSafe.Client/app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  $URL = "http://localhost:3000/"
+  $URL = "http://localhost:3001/"
+  $gUsername = ""
+  $gPubkey_user = ""
+  $gPrivkey_user = ""
 end
